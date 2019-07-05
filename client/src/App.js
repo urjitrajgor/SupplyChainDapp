f1c256a86959dc23b47d3e58844d63d8842fcce8
Urjit Rajgor <urjit.rajgor@gmail.com>
2019-07-06 01:09:29 +0530

    improve track assets

--- client/src/App.js
+++ client/src/App.js
@@ -57,36 +57,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">         
-        <div class="jumbotron">
+        <div className="jumbotron">
           <h2>Supplychain Tracking System</h2>
           </div>
-          <div class= "col-sm-12 text-center" style={{ textAlign: 'center' }}>
+          <div className= "col-sm-12 text-center" style={{ textAlign: 'center' }}>
             <label ><b>Create Asset:</b></label>
             <input type="text" id="assetName" placeholder=" Asset Name" required />
             <input type="text" id="assetDescription" placeholder="Description" required />
             <input type="number" id="assetUuid" placeholder="UUID" required />
-            <button class = "btn-primary" onClick={this.newAssets} id="createAsset">Create Asset</button>
+            <button className = "btn-primary" onClick={this.newAssets} id="createAsset">Create Asset</button>
           </div>
                                                                                                                                                             
-          <div class= "col-sm-12 text-center" style={{ textAlign: 'center' }}>
+          <div className= "col-sm-12 text-center" style={{ textAlign: 'center' }}>
             <label><b>Transfer Asset:</b></label>
             <input type="text" id="address" placeholder="To address" required />
             <input type="number" id="Uuid" placeholder="UUID" required />
-            <button class="btn-primary" onClick={this.transferAssets} id="transferAsset">Transfer Asset</button>
+            <button className="btn-primary" onClick={this.transferAssets} id="transferAsset">Transfer Asset</button>
           </div>
 
-          <div class= "col-sm-12 text-center" style={{ textAlign: 'center' }}>
+          <div className= "col-sm-12 text-center" style={{ textAlign: 'center' }}>
             <label><b>Track Asset Details:</b></label>
             <input type="number" id="trackUuid" placeholder="UUID" required />
-            <button class="btn-primary" onClick={this.trackAsset} id="trackAsset">Track Asset Details</button>
+            <button className="btn-primary" onClick={this.trackAsset} id="trackAsset">Track Asset Details</button>
           </div>
 
           <div id="response text-center" style={{ textalign:'center'}}>
-            
+            {console.log(this.state.assets)}
             <p><b>Name:</b><span id="name">{this.state.assets[0] === null? '': this.state.assets[0]}</span></p>
             <p><b>Description:</b><span id="description">{this.state.assets[1] === null? '': this.state.assets[1]}</span></p>
             <p><b>Manufacturer:</b><span id="manufacturer">{this.state.assets[2] === null? '': this.state.assets[2]}</span></p>
-            <p><b>Current Location:</b><span id="location">{this.state.assets[2] === null? '': this.state.assets[2]}</span></p>
+            <p><b>Current Location:</b><span id="location">{this.state.assets[3] === null? '': this.state.assets[3]}</span></p>
           </div>
       </div>
     );
--- contracts/AssetsTracker.sol
+++ contracts/AssetsTracker.sol
@@ -25,17 +25,19 @@ contract AssetsTracker{
         assetsStore[_uuid].description = _description;
         assetsStore[_uuid].manufacturer = msg.sender;
         assetsStore[_uuid].initialized = true;
+        locations[_uuid].location = msg.sender;
+        locations[_uuid].uuid = _uuid;
         emit AssetsCreate(msg.sender, _uuid, msg.sender);
     }
 
     function transferAssets(string memory _uuid, address _to ) public{
         require(locations[_uuid].location == msg.sender, "You are not authorised to transfer this assets");
         locations[_uuid] = Tracking(_to, _uuid);
-        emit AssetsTransfer(msg.sender, msg.sender, _uuid);
+        emit AssetsTransfer(msg.sender, _to, _uuid);
     }
 
-    function getAssetDetails(string memory _uuid) public view returns(string memory, string memory, address){
-        return(assetsStore[_uuid].name, assetsStore[_uuid].description, assetsStore[_uuid].manufacturer);
+    function getAssetDetails(string memory _uuid) public view returns(string memory, string memory, address, address){
+       return(assetsStore[_uuid].name, assetsStore[_uuid].description, assetsStore[_uuid].manufacturer, locations[_uuid].location);
     }
 
     function AssetLocation(string memory _uuid)public view returns(address){
